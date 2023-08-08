24241e470b115ccec1db2aa3e0e464e209b6784a
gtopinio <mctopinio@up.edu.ph>
2023-08-08 21:16:10 +0800

    release: initial commit

---
+++ src/main/java/com/github/gtopinio/progresspal/ProgresspalApplication.java
@@ -0,0 +1,13 @@
+package com.github.gtopinio.progresspal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgresspalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgresspalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gtopinio/progresspal/ProgresspalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gtopinio.progresspal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgresspalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
