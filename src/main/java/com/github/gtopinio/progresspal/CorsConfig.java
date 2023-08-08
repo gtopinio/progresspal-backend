6d442ee04e71b8dd9835ea77e6f90ec8eb09b1d1
gtopinio <mctopinio@up.edu.ph>
2023-08-08 22:10:50 +0800

    update: add cors configuration

---
+++ src/main/java/com/github/gtopinio/progresspal/CorsConfig.java
@@ -0,0 +1,13 @@
+package com.github.gtopinio.progresspal;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
