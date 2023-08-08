c38940d908546716cc135896e65853c605c99937
gtopinio <mctopinio@up.edu.ph>
2023-08-08 22:08:13 +0800

    update: configure app properties and pom.xml, test simple endpoint to app

--- src/main/java/com/github/gtopinio/progresspal/ProgresspalApplication.java
+++ src/main/java/com/github/gtopinio/progresspal/ProgresspalApplication.java
@@ -2,12 +2,19 @@ package com.github.gtopinio.progresspal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProgresspalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgresspalApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
 }
